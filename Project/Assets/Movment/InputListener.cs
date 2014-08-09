80b75475ad4a3d9c549398d42ded786192ca30be
Kaue Aftimus Rosa <kaue@kaueaftimus.com>
2014-08-09 15:56:00 -0700

    Reading input as a Vector 2

---
+++ Project/Assets/Movment/InputListener.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputListener : MonoBehaviour {
+
+	[SerializeField] private PlayerNavigation pn = null;
+	
+	void Update () {
+		pn.Navigate(new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical")));
+	}
+}
---
+++ Project/Assets/Movment/PlayerNavigation.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerNavigation : MonoBehaviour {
+
+	public void Navigate(Vector2 _direction) {
+		print (_direction);
+	}
+}
