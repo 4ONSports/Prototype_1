5edee98ab9db5d256cda25abcafb65b9a93621ff
grantkowalewski <kowalewski.grant@gmail.com>
2014-08-09 16:01:16 -0700

    Adding assets for AI development
    All assets/scenes that I will be working on will be kept in GrantAssets
    to ease access. Currently have basic ChaserAI movement; no chasing.
    
    TBD: chasing, additional movement, character rotation

---
+++ Project/Assets/GrantAssets/Scripts/ChaserAI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChaserAI : MonoBehaviour {
+
+	// Chaser will check for player in vicinity/vision range
+	// Chaser will move forward and bump off walls if no player around
+
+	public float moveSpeed;
+	public int moveAngle;
+	public int visionRange = 5;
+
+	// Use this for initialization
+	void Start () {
+		transform.Rotate (0,Time.deltaTime,0, Space.Self);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (transform.rotation * Time.deltaTime * moveSpeed, Space.World);
+	}
+}
---
+++ Project/Assets/GrantAssets/Scripts/FakePlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FakePlayer : MonoBehaviour {
+
+	public int degPerSec = 20;
+	public Transform rotateAround;
+
+	void Start() {
+	}
+
+	void Update() {
+		transform.RotateAround(Vector3.up, Vector3.up, degPerSec * Time.deltaTime);
+	}
+
+}
