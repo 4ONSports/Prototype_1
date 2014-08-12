5152c20be665562a2dd5e2a3b2d3b73289cc1512
Kaue Aftimus Rosa <kaue.aftimus@gmail.com>
2014-08-11 23:11:54 -0700

    first iteration of mobile gamepad

---
+++ Project/Assets/Movment/DragJoystick.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragJoystick : MonoBehaviour {
+
+	private Vector3 screenPoint = Vector3.zero;
+	private Vector3 offset;
+	private float _lockedYPosition;
+	[SerializeField] private Camera uiCamera = null;
+
+	void OnMouseDown() {
+		_lockedYPosition = screenPoint.y;
+		offset = gameObject.transform.position - uiCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));	
+	}
+
+	void OnMouseDrag() 	{
+		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 curPosition = uiCamera.ScreenToWorldPoint(curScreenPoint) + offset;
+		transform.position = new Vector3 (curPosition.x,curPosition.y,transform.position.z);
+		transform.localPosition = new Vector3 (Mathf.Clamp(transform.localPosition.x,-0.5f,0.5f), Mathf.Clamp(transform.localPosition.y,-0.5f,0.5f),transform.localPosition.z);
+	}
+	
+	void OnMouseUp() {
+		transform.localPosition = Vector3.zero;
+	}
+}
