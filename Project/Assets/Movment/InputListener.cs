81c90d48838c222005a36df5e54e34b82aa26823
Kaue Aftimus Rosa <kaue.aftimus@gmail.com>
2014-08-12 16:28:54 -0700

    mobile controls second iteration and integration with movment

--- Project/Assets/Movment/InputListener.cs
+++ Project/Assets/Movment/InputListener.cs
@@ -4,8 +4,17 @@ using System.Collections;
 public class InputListener : MonoBehaviour {
 
 	[SerializeField] private PlayerNavigation pn = null;
+	[SerializeField] private MobileControl mc = null;
 	
 	void Update () {
-		pn.Navigate(new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical")));
+		if (mc) {
+			MoveInput(mc.normal);
+		} else {
+			MoveInput (new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical")));
+		}
+	}
+
+	void MoveInput (Vector2 dir) {
+		pn.Navigate (dir);
 	}
 }
---
+++ Project/Assets/Movment/MobileControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(BoxCollider))]
+
+public class MobileControl : MonoBehaviour {
+
+	private Vector3 screenPoint = Vector3.zero;
+	private Vector3 offset;
+
+	[SerializeField] private Camera uiCamera = null;
+	[SerializeField] Transform joystickTop  = null;
+
+	public Vector3 normal;
+
+	void OnMouseDown() {
+		offset = joystickTop.position - uiCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));	
+	}
+
+	void OnMouseDrag() 	{
+		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 curPosition = uiCamera.ScreenToWorldPoint(curScreenPoint) + offset;
+		joystickTop.position = new Vector3 (curPosition.x,curPosition.y,joystickTop.position.z);
+		normal = Vector3.Normalize (joystickTop.localPosition);
+		joystickTop.localPosition = normal * 0.5f;
+	}
+	
+	void OnMouseUp() {
+		joystickTop.localPosition = Vector3.zero;
+		normal = Vector3.zero;
+	}
+}
