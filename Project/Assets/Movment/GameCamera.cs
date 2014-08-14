725671dbfed03ea3c927de85876e864fb656a0d9
Kaue Aftimus Rosa <kaue.aftimus@gmail.com>
2014-08-14 11:46:01 -0700

    Adding GameCamera.cs, camera follows the midpoint of its targets and able to switch between back view and side view

---
+++ Project/Assets/Movment/GameCamera.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+public class GameCamera : MonoBehaviour {
+
+	[HideInInspector] public TypeOfFieldView fieldView = TypeOfFieldView.BACK_VIEW;
+
+	[SerializeField] private List<Transform> targetTransforms = null;
+	[SerializeField] private Vector3 offsetBack = new Vector3(0,7,-9);
+	[SerializeField] private Vector3 offsetSide = new Vector3(-14,7,0);
+
+	private Vector3 offset;
+
+
+	void Start() {
+		//TODO:Remove this switch
+		switch(fieldView) {
+		case TypeOfFieldView.BACK_VIEW:
+			offset = offsetBack;
+			transform.rotation = Quaternion.Euler(30,0,0);
+			break;
+		case TypeOfFieldView.SIDE_VIEW:
+			offset = offsetSide;
+			transform.rotation = Quaternion.Euler(30,90,0);
+			break;
+		}
+	}
+
+
+	void Update () {
+		Vector3 mediumPoint = Vector3.zero;
+		Vector3 sum = Vector3.zero;
+		//TODO: Get rid of this if
+		if(targetTransforms.Count > 1) {
+			foreach (Transform t in targetTransforms) {
+				sum += t.transform.position;
+				mediumPoint = sum * 0.5f;
+			}
+		} else {
+			mediumPoint = targetTransforms[0].transform.position;
+		}
+		transform.position = Vector3.MoveTowards (transform.position,mediumPoint  + offset,5f);
+	}
+}
--- Project/Assets/Movment/InputListener.cs
+++ Project/Assets/Movment/InputListener.cs
@@ -1,14 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum TypeOfFieldView{
+	BACK_VIEW,
+	SIDE_VIEW
+}
+
 public class InputListener : MonoBehaviour {
 
+	
+	[SerializeField] private TypeOfFieldView fieldView = TypeOfFieldView.BACK_VIEW;
 	[SerializeField] private PlayerNavigation pn = null;
 	[SerializeField] private MobileControl mc = null;
+
+	void Awake() {
+		Camera.main.GetComponent<GameCamera> ().fieldView = this.fieldView;
+	}
 	
 	void Update () {
 		if (mc) {
-			MoveInput(mc.normal);
+			if(fieldView == TypeOfFieldView.BACK_VIEW) MoveInput(mc.normal);
+			else MoveInput(new Vector2(mc.normal.y,-mc.normal.x));
 		} else {
 			MoveInput (new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical")));
 		}
