4da8cdc93400b5dfd2ff58daac494b87b7ef845e
Kaue Aftimus Rosa <kaue.aftimus@gmail.com>
2014-08-21 13:38:50 -0700

    major refactor on GameCamera, adding startegy design pattern

--- Project/Assets/Scripts/GameCamera.cs
+++ Project/Assets/Scripts/GameCamera.cs
@@ -2,60 +2,113 @@
 using System.Collections;
 using System.Collections.Generic;
 
-
-
 public class GameCamera : MonoBehaviour {
 
-	[SerializeField] private Transform debugTransform = null;
-	[SerializeField] private List<Transform> targetTransforms = null;
-	[SerializeField] private Vector3 offsetBack = new Vector3(0,7,-9);
-	[SerializeField] private Vector3 offsetSide_Left = new Vector3(-14,7,0);
-	[SerializeField] private Vector3 offsetSide_Right = new Vector3(14,7,0);
-	[SerializeField] private Vector3 back_View = new Vector3(30,0,0);
-	[SerializeField] private Vector3 side_View_Left = new Vector3(30,90,0);
-	[SerializeField] private Vector3 side_View_Right = new Vector3(30,-90,0);
+	public Transform debugTransform = null;
+	public List<Transform> targetTransforms = null;
+	public Vector3 offsetBack = new Vector3(0,7,-9);
+	public Vector3 offsetSide_Left = new Vector3(-14,7,0);
+	public Vector3 offsetSide_Right = new Vector3(14,7,0);
+	public Vector3 back_View = new Vector3(30,0,0);
+	public Vector3 side_View_Left = new Vector3(30,90,0);
+	public Vector3 side_View_Right = new Vector3(30,-90,0);
 
 	private Vector3 offset;
 	private InputListener match_IL;
-
+	private Dictionary<TypeOfFieldView, GameCameraBehaviour> cameraBehaviours = new Dictionary<TypeOfFieldView, GameCameraBehaviour>();
 
 	void Start() {
 		match_IL = GameObject.Find("GameMatch").GetComponent<InputListener> ();
-	}
-	
-	void Update () {
+		cameraBehaviours.Add (TypeOfFieldView.BACK_VIEW,new GameCameraBehaviourBack(this));
+		cameraBehaviours.Add (TypeOfFieldView.SIDE_VIEW_LEFT,new GameCameraBehaviourSideLeft(this));
+		cameraBehaviours.Add (TypeOfFieldView.SIDE_VIEW_RIGHT,new GameCameraBehaviourSideRight(this));
 	}
 
 	void LateUpdate () {
-		//TODO:Remove this switch
-		switch(match_IL.fieldView) {
-		case TypeOfFieldView.BACK_VIEW:
-			offset = offsetBack;
-			transform.rotation = Quaternion.Euler(back_View.x, back_View.y, back_View.z);
-			break;
-		case TypeOfFieldView.SIDE_VIEW_LEFT:
-			offset = offsetSide_Left;
-			transform.rotation = Quaternion.Euler(side_View_Left.x, side_View_Left.y, side_View_Left.z);
-			break;
-		case TypeOfFieldView.SIDE_VIEW_RIGHT:
-			offset = offsetSide_Right;
-			transform.rotation = Quaternion.Euler(side_View_Right.x, side_View_Right.y, side_View_Right.z);
-			break;
-		}
+		//Deal with Camera switches
+		//TODO: make this triggered by something
+		cameraBehaviours [match_IL.fieldView].OnCameraSwitch ();
+		//Deal with Camera Logic
+		cameraBehaviours [match_IL.fieldView].OnCameraLogic ();
+		//Debug
+		if(GameDebug.CheckIfActiveFeature(DebugFeature.DEBUG_CAMERA_MIDDLE_POINT))
+			cameraBehaviours [match_IL.fieldView].OnDebugMiddlePoint ();
+	}
+}
 
+public abstract class GameCameraBehaviour {
+	public GameCamera gc;
+	public Vector3 offset;
 
-		Vector3 mediumPoint = Vector3.zero;
+	abstract public void OnCameraSwitch();
+	abstract public void OnCameraLogic();
+
+	public Vector3 GetMediumPoint() {
 		Vector3 sum = Vector3.zero;
 		//TODO: Get rid of this if
-		if(targetTransforms.Count > 1) {
-			foreach (Transform t in targetTransforms) {
+		if(gc.targetTransforms.Count > 1) {
+			foreach (Transform t in gc.targetTransforms) {
 				sum += t.transform.position;
 			}
-			mediumPoint = sum * 0.5f;
+			return sum * 0.5f;
 		} else {
-			mediumPoint = targetTransforms[0].transform.position;
+			return gc.targetTransforms[0].transform.position;
 		}
-		if(GameDebug.CheckIfActiveFeature(DebugFeature.DEBUG_CAMERA_MIDDLE_POINT)) debugTransform.position = mediumPoint;
-		transform.position = Vector3.MoveTowards (transform.position,mediumPoint  + offset,10f* Time.deltaTime);
+	}
+
+	public void OnDebugMiddlePoint() {
+		gc.debugTransform.position = GetMediumPoint ();
+	}
+
+
+}
+//BACK BEHAVIOUR
+public class GameCameraBehaviourBack : GameCameraBehaviour {
+
+	public GameCameraBehaviourBack (GameCamera _gc) {
+		gc = _gc;
+	}
+
+	override public void OnCameraSwitch() {
+		gc.transform.rotation = Quaternion.Euler(gc.back_View.x, gc.back_View.y, gc.back_View.z);
+		offset = gc.offsetBack;
+	}
+
+	override public void OnCameraLogic() {
+		gc.transform.position = Vector3.MoveTowards (gc.transform.position,GetMediumPoint()  + offset,10f* Time.deltaTime);
+	}
+}
+
+//SIDE LEFT BEHAVIOUR
+public class GameCameraBehaviourSideLeft : GameCameraBehaviour {
+	
+	public GameCameraBehaviourSideLeft (GameCamera _gc) {
+		this.gc = _gc;
+	}
+
+	override public void OnCameraSwitch() {
+		gc.transform.rotation = Quaternion.Euler(gc.side_View_Left.x, gc.side_View_Left.y, gc.side_View_Left.z);
+		offset = gc.offsetSide_Left;
+	}
+
+	override public void OnCameraLogic() {
+		gc.transform.position = Vector3.MoveTowards (gc.transform.position,GetMediumPoint()  + offset,10f* Time.deltaTime);
+	}
+}
+
+//SIDE RIGHT BEHAVIOUR
+public class GameCameraBehaviourSideRight : GameCameraBehaviour {
+	
+	public GameCameraBehaviourSideRight (GameCamera _gc) {
+		this.gc = _gc;
+	}
+
+	override public void OnCameraSwitch() {
+		gc.transform.rotation = Quaternion.Euler(gc.side_View_Right.x, gc.side_View_Right.y, gc.side_View_Right.z);
+		offset = gc.offsetSide_Right;
+	}
+
+	override public void OnCameraLogic() {
+		gc.transform.position = Vector3.MoveTowards (gc.transform.position,GetMediumPoint()  + offset,10f* Time.deltaTime);
 	}
 }
